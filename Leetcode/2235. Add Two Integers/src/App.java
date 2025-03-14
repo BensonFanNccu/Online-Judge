58f5ea9c992245269cf3f2827e2fbd0e99cb636f
BensonFanNccu <110306044@g.nccu.edu.tw>
2025-03-14 12:04:35 +0800

    2235: Init commit

---
+++ Leetcode/2235. Add Two Integers/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*
+         * Given two integers num1 and num2, return the sum of the two integers.
+         * 
+         * Example:
+         * Input: num1 = 12, num2 = 5
+         * Output: 17
+         */
+
+        Solution sol = new Solution();
+
+        System.out.println(sol.sum(5, 12));
+        System.out.println(sol.sum(-10, 4));
+    }
+}
---
+++ Leetcode/2235. Add Two Integers/src/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int sum(int num1, int num2){
+        /*
+         * Straight Forward Solution
+         * Time: O(1)
+         * Space: O(1)
+         */
+        
+        return num1 + num2;
+    }
+}
