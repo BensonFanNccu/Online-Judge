1394020dcc0d8402dccf9ba2ec3c486d5fbf5542
BensonFanNccu <110306044@g.nccu.edu.tw>
2025-03-14 10:47:31 +0800

    0021: Init commit

---
+++ Leetcode/0021. Merge Two Sorted Lists/src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*
+         * You are given the heads of two sorted linked lists list1 and list2.
+         * Merge the two lists into one sorted list. 
+         * The list should be made by splicing together the nodes of the first two lists.
+         * Return the head of the merged linked list.
+         * 
+         * Example:
+         * Input: list1 = [1,2,4], list2 = [1,3,4]
+         * Output: [1,1,2,3,4,4]
+         */
+        
+        Solution sol = new Solution();
+
+        ListNode case1 = new ListNode(new int[] {1, 3, 5});
+        ListNode case2 = new ListNode(new int[] {1, 2, 4});
+
+        sol.mergeTwoLists(case1, case2).printNodes();
+    }
+}
