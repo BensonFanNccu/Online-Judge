1f73ced19653a289e660b33efc3c0c1356b2d4f4
BensonFanNccu <110306044@g.nccu.edu.tw>
2025-03-18 22:15:06 +0800

    0042: Add Requirements

--- Leetcode/0042. Trapping Rain Water/src/App.java
+++ Leetcode/0042. Trapping Rain Water/src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        /*
+         * Given n non-negative integers representing an elevation map 
+         * where the width of each bar is 1, compute how much water it can trap after raining.
+         * 
+         * Example:
+         * Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+         * Output: 6
+         */
+        
         Solution sol = new Solution();
 
         int[] case1 = new int[] {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
